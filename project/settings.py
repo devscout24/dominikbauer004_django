00a20a36928a9eea54c3f33e5bdf4a374b36bdf0
Azizur Rahaman <your-email@example.com>
2025-08-29 06:34:54 +0600

    change

--- project/settings.py
+++ project/settings.py
@@ -190,7 +190,6 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = BASE_DIR / "staticfiles" 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
